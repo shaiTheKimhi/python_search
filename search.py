b200dcff23c3af4717b2f5b53971c2d07227ec4d
shaiKemhi <shaikimhi221199@gmail.com>
2018-08-11 22:18:12 +0300

    initial commit

---
+++ search.py
@@ -0,0 +1,20 @@
+import os 
+import sys
+def searchInDir(fileName,dirName,list):
+    dirs = os.listdir(dirName)
+    for f in dirs:
+        if(f.lower()==fileName):
+            list.append(dirName+"/"+fileName)
+        if(len(f.split("."))==1):
+            searchInDir(fileName,f,list)
+    return list
+def searchTree(fileName,Dir):
+    l=[]
+    l = searchInDir(fileName,Dir,l)
+    print(l)
+    # change to return
+    dir = os.getcwd()
+    fileName = sys.argv[1]
+    searchTree(fileName, dir)
+    print("done")
+    nothing = input()
